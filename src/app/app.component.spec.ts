eaa159de84559db220b33820d7621a194d019839
Gizelly Silva <gizelly.silva@playkids.com>
2022-02-28 18:11:22 -0400

    feat: adding like component and adjusts test on app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe(AppComponent.name, () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tests-with-angular app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('tests-with-angular');
   });
 });
